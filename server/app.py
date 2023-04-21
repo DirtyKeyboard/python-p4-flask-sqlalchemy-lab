132e909b73052a5772cb5f9352ffb52a2d6c9f79
DirtyKeyboard <andrewxx000222@icloud.com>
2023-04-21 18:13:41 -0500

    d

--- server/app.py
+++ server/app.py
@@ -19,16 +19,56 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
+    if not animal:
+        return make_response('<h1>404! Animal not found :(</h1>', 404)
+
+    keeper = Zookeeper.query.filter(Zookeeper.id == animal.zookeeper_id).first()
+    closure = Enclosure.query.filter(Enclosure.id == animal.enclosure_id).first()
+    body = f"""
+        <ul>ID: {animal.id}</ul>
+        <ul>Name: {animal.name}</ul>
+        <ul>Species: {animal.species}</ul>
+        <ul>Zookeeper: {keeper.name}</ul>
+        <ul>Enclosure: {closure.environment}</ul>
+        """
+    return make_response(body, 200)
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    keeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+    if not keeper:
+        return make_response("<h1>404!! Zookeeper not found</h1>", 404)
+    
+    body = f"""<ul>ID: {keeper.id}</ul>
+                <ul>Name: {keeper.name}</ul>
+                <ul>Birthday: {keeper.birthday}</ul>
+    """
+    for el in keeper.animals:
+        body = f"{body}<ul>Animal: {el.name}</ul>"
 
+    return make_response(body, 200)
+    
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    closure = Enclosure.query.filter(Enclosure.id == id).first()
+    if not closure:
+        return make_response("<h1>404!! Enclosure not found</h1>", 404)
 
+    body = f"""
+    <ul>ID: {closure.id}</ul>
+    <ul>Environment: {closure.environment}</ul>
+    <ul>Open to Visitors: {closure.open_to_visitors}</ul>
+    """
+    for el in closure.animals:
+        body = f"{body}<ul>Animal:{el.name}</ul>"
 
+    return make_response(body, 200)
+    
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+"""
+Your application should contain three views: animal_by_id, zookeeper_by_id, and enclosure_by_id. Their routes should be animal/<int:id>, zookeeper/<int:id>, and enclosure/<int:id>, respectively.
+Each view should display all attributes as line items (ul). If there is a one-to-many relationship, each of the many should have its own line item.
+"""
--- server/models.py
+++ server/models.py
@@ -9,15 +9,22 @@ db = SQLAlchemy(metadata=metadata)
 
 class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
-
+    name = db.Column(db.String)
+    birthday = db.Column(db.Date)
+    animals = db.relationship('Animal', backref='zookeeper')
     id = db.Column(db.Integer, primary_key=True)
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
-
+    environment = db.Column(db.String)
+    open_to_visitors = db.Column(db.Boolean)
+    animals = db.relationship('Animal', backref='enclosure')
     id = db.Column(db.Integer, primary_key=True)
 
 class Animal(db.Model):
     __tablename__ = 'animals'
-
+    name = db.Column(db.String)
+    species = db.Column(db.String)
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
     id = db.Column(db.Integer, primary_key=True)
